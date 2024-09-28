8be3c5112afbfd5cea1b626ed1c5e477c52c5971
vijayascanscheyyar <cheyyarvijayascans@gmail.com>
2024-09-28 14:51:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import Confetti from "./Confetti";
+import logo from "./assets/TrendingTag-logo.png";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Confetti />
+      <header className="App-header">
+        <img src={logo} alt="Logo" className="logo" />
+        <p style={{ marginTop: "50px", marginBottom: "50px", color: "black" }}>
+          "Big Things Are Coming! Our Website Launches Soon. Stay Tuned!"
+        </p>
+        <div>
+          <a
+            target="_blank"
+            href={
+              "https://www.facebook.com/profile.php?id=61566195509486&sk=about"
+            }
+            class="fa-brands fa-facebook"
+            rel="noopener noreferrer"
+          >
+            {" "}
+          </a>
+          <a
+            target="_blank"
+            href={
+              "https://www.instagram.com/thetrendingtag/?utm_source=ig_web_button_share_sheet"
+            }
+            class="fa-brands fa-instagram"
+            rel="noopener noreferrer"
+          >
+            {" "}
+          </a>
+          <a
+            target="_blank"
+            href={
+              "https://www.instagram.com/thetrendingtag/?utm_source=ig_web_button_share_sheet"
+            }
+            class="fa-brands fa-twitter"
+            rel="noopener noreferrer"
+          >
+            {" "}
+          </a>
+        </div>
+        <div className="contact-details">
+          <h2>Contact Us</h2>
+          <p>Email: thetrendingtag2024@gmail.com</p>
+          <p>Phone: +91 9498479848</p>
+        </div>
+      </header>
+      <footer className="App-footer"></footer>
+    </div>
+  );
+};
+
+export default App;
